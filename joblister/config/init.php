c33d13b507a96f71a79904f1b30406b889f89bfd
Ling <lingduke@gmail.com>
2020-02-15 01:18:38 -0500

    adding second project

---
+++ joblister/config/init.php
@@ -0,0 +1,16 @@
+<?php
+//start session
+session_start();
+
+
+//config file
+require_once 'config.php';
+
+//inclued helpers
+require_once 'helpers/system_helper.php';
+
+//autoloader
+function __autoload($class_name)
+{
+    require_once 'lib/' . $class_name . '.php';
+}
